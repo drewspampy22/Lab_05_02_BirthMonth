45cc2afb9068bd7b6d378383859ac5d9a1eae142
drewspampy22 <drewspampy22@gmail.com>
2022-09-19 20:14:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int BirthMonth= 3;
+
+       if (BirthMonth > 0 && BirthMonth <= 12 )
+        {
+            System.out.println("Your birth month is, "+ BirthMonth);
+        }
+        else
+        {
+            System.out.println("Invalid Input of: " + BirthMonth);
+        }
+
+
+
+
+
+
+
+    }
+}
